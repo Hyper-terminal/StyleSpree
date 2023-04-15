f0f50cc1f727c20649529b9e7ea7d665bb300ae8
Hyper-terminal <abhisharma001001001@gmail.com>
2023-04-15 13:16:10 +0530

    image zoom functionality added

--- src/pages/product-details/ImageInfo.jsx
+++ src/pages/product-details/ImageInfo.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import InnerImageZoom from "react-inner-image-zoom";
+import "react-inner-image-zoom/lib/InnerImageZoom/styles.min.css";
 import SwiperCore, { Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -5,25 +8,51 @@ import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
 import useMediaQuery from "../../hooks/useMediaQuery";
 import classes from "./image-info.module.css";
+
 SwiperCore.use([Pagination]);
+
+// Add some styles to center the images within the SwiperSlide components
+const swiperSlideStyles = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
 
 const ImageInfo = ({ product }) => {
   const isMobile = useMediaQuery("(max-width: 770px)");
   const { images } = product;
+
   return (
     <div className={classes.container}>
       {!isMobile &&
         images.map(
           (item) =>
-            item.src && <img key={item.view} src={item.src} alt={item.view} />
+            item.src && (
+              <div className={classes.subcontainer}>
+                <InnerImageZoom
+                  key={item.view}
+                  src={item.src}
+                  zoomSrc={item.src}
+                  alt={item.view}
+                  zoomScale={0.9}
+                />
+              </div>
+            )
         )}
       {isMobile && (
-        <Swiper pagination>
+        <Swiper style={{ height: "100%" }} pagination>
           {images.map(
             (item) =>
               item.src && (
-                <SwiperSlide key={item.view}>
-                  <img src={item.src} alt={item.view} />
+                <SwiperSlide key={item.view} style={swiperSlideStyles}>
+                  <InnerImageZoom
+                    key={item.view}
+                    src={item.src}
+                    zoomSrc={item.src}
+                    alt={item.view}
+                    zoomScale={1.1}
+                    fullscreenOnMobile
+                  />
                 </SwiperSlide>
               )
           )}
